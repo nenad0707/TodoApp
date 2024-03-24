f8358e57a4ccf2096d8c26b68e8a2b1f15a8e861
nenad0707 <risticnenad.vr@gmail.com>
2024-03-24 13:40:06 +0100

    Refactor TodosController class

--- TodoApi/Controllers/TodosController.cs
+++ TodoApi/Controllers/TodosController.cs
@@ -1,43 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace TodoApi.Controllers
+namespace TodoApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TodosController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TodosController : ControllerBase
+    // GET: api/Todos
+    [HttpGet]
+    public IEnumerable<string> Get()
     {
-        // GET: api/<TodosController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        return new string[] { "value1", "value2" };
+    }
 
-        // GET api/<TodosController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+    // GET api/Todos/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
 
-        // POST api/<TodosController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+    // POST api/Todos
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
 
-        // PUT api/<TodosController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+    // PUT api/Todos/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
 
-        // DELETE api/<TodosController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+    // DELETE api/Todos/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
     }
 }
