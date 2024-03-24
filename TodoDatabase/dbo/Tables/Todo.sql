2f17b099de1b10319fed1b0910ac92b4bbe2a9c8
nenad0707 <risticnenad.vr@gmail.com>
2024-03-24 16:56:24 +0100

    Added database priject

--- TodoDatabase/dbo/Tables/Todo.sql
+++ TodoDatabase/dbo/Tables/Todo.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[Todo]
 (
-    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Task] NVARCHAR(50) NULL, 
-    [AssignedTo] NVARCHAR(50) NULL, 
-    [IsCompleted] BIT NULL
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Task] NVARCHAR(100) NOT NULL,
+	[Description] NVARCHAR(1000) NOT NULL,
+	[AssignedTo] INTEGER NOT NULL,
+	[IsCompleted] BIT NOT NULL
 )
