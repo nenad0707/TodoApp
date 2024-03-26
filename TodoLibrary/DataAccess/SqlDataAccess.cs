21a37b88d38fce4721fa4e2dcd511e65b4a4acd6
nenad0707 <risticnenad.vr@gmail.com>
2024-03-26 15:15:07 +0100

    Refactor SqlDataAccess class and add ISqlDataAccess interface

--- TodoLibrary/DataAccess/SqlDataAccess.cs
+++ TodoLibrary/DataAccess/SqlDataAccess.cs
@@ -5,55 +5,55 @@ using Microsoft.Extensions.Configuration;
 
 namespace TodoLibrary.DataAccess;
 
-public class SqlDataAccess
+public class SqlDataAccess : ISqlDataAccess
 {
 
-  private readonly IConfiguration _config;
+    private readonly IConfiguration _config;
 
-  /// <summary>
-  /// Initializes a new instance of the <see cref="SqlDataAccess"/> class.
-  /// </summary>
-  /// <param name="config">The configuration object.</param>
-  public SqlDataAccess(IConfiguration config)
-  {
-    this._config = config;
-  }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SqlDataAccess"/> class.
+    /// </summary>
+    /// <param name="config">The configuration object.</param>
+    public SqlDataAccess(IConfiguration config)
+    {
+        this._config = config;
+    }
 
-  /// <summary>
-  /// Loads data from the database using a stored procedure.
-  /// </summary>
-  /// <typeparam name="T">The type of data to load.</typeparam>
-  /// <typeparam name="U">The type of parameters for the stored procedure.</typeparam>
-  /// <param name="storedProcedure">The name of the stored procedure.</param>
-  /// <param name="parameters">The parameters for the stored procedure.</param>
-  /// <param name="connectionStringName">The name of the connection string in the configuration.</param>
-  /// <returns>A list of loaded data.</returns>
-  public async Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
-  {
-    string connectionString = _config.GetConnectionString(connectionStringName)!;
+    /// <summary>
+    /// Loads data from the database using a stored procedure.
+    /// </summary>
+    /// <typeparam name="T">The type of data to load.</typeparam>
+    /// <typeparam name="U">The type of parameters for the stored procedure.</typeparam>
+    /// <param name="storedProcedure">The name of the stored procedure.</param>
+    /// <param name="parameters">The parameters for the stored procedure.</param>
+    /// <param name="connectionStringName">The name of the connection string in the configuration.</param>
+    /// <returns>A list of loaded data.</returns>
+    public async Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+    {
+        string connectionString = _config.GetConnectionString(connectionStringName)!;
 
-    using IDbConnection connection = new SqlConnection(connectionString);
+        using IDbConnection connection = new SqlConnection(connectionString);
 
-    var rows = await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+        var rows = await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
 
-    return rows.ToList();
-  }
+        return rows.ToList();
+    }
 
-  /// <summary>
-  /// Saves data to the database using a stored procedure.
-  /// </summary>
-  /// <typeparam name="T">The type of parameters for the stored procedure.</typeparam>
-  /// <param name="storedProcedure">The name of the stored procedure.</param>
-  /// <param name="parameters">The parameters for the stored procedure.</param>
-  /// <param name="connectionStringName">The name of the connection string in the configuration.</param>
-  /// <returns>A task representing the asynchronous operation.</returns>
-  public Task SaveData<T>(string storedProcedure, T parameters, string connectionStringName)
-  {
-    string connectionString = _config.GetConnectionString(connectionStringName)!;
+    /// <summary>
+    /// Saves data to the database using a stored procedure.
+    /// </summary>
+    /// <typeparam name="T">The type of parameters for the stored procedure.</typeparam>
+    /// <param name="storedProcedure">The name of the stored procedure.</param>
+    /// <param name="parameters">The parameters for the stored procedure.</param>
+    /// <param name="connectionStringName">The name of the connection string in the configuration.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public Task SaveData<T>(string storedProcedure, T parameters, string connectionStringName)
+    {
+        string connectionString = _config.GetConnectionString(connectionStringName)!;
 
-    using IDbConnection connection = new SqlConnection(connectionString);
+        using IDbConnection connection = new SqlConnection(connectionString);
 
-    return connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
-  }
+        return connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
 }
 
