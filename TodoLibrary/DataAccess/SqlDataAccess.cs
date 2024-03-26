7c91d68115bc21b69ae8bed7712a5710565d8e48
nenad0707 <risticnenad.vr@gmail.com>
2024-03-26 14:41:26 +0100

    Add IConfiguration dependency to SqlDataAccess class and Dapper library nuget package

--- TodoLibrary/DataAccess/SqlDataAccess.cs
+++ TodoLibrary/DataAccess/SqlDataAccess.cs
@@ -1,5 +1,13 @@
-﻿namespace TodoLibrary.DataAccess;
+﻿using Microsoft.Extensions.Configuration;
+
+namespace TodoLibrary.DataAccess;
 
 public class SqlDataAccess
 {
+  private readonly IConfiguration _config;
+
+  public SqlDataAccess(IConfiguration config)
+  {
+    this._config = config;
+  }
 }
