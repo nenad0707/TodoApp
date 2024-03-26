4ef548fe125c23d73183bbafc6c6a7da3868dbbc
nenad0707 <risticnenad.vr@gmail.com>
2024-03-26 15:35:02 +0100

    Add connection string and TodData class

---
+++ TodoLibrary/DataAccess/TodoData.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using TodoLibrary.DataAccess;
+using TodoLibrary.Models;
+
+namespace TodoLibrary;
+
+public class TodoData
+{
+  private readonly ISqlDataAccess _sql;
+
+  public TodoData(ISqlDataAccess sql)
+  {
+    this._sql = sql;
+  }
+
+  public Task<List<TodoModel>> GetAllAssigned(int assignedTo)
+  {
+    return _sql.LoadData<TodoModel, dynamic>("dbo.spTodos_GetAllAssigned", new { AssignedTo = assignedTo }, "Default");
+  }
+}
