1b16dbbf682c8fc81a752f1c54611b202851a62d
nenad0707 <risticnenad.vr@gmail.com>
2024-03-26 14:59:25 +0100

    Add constructor and method documentation

--- TodoLibrary/DataAccess/SqlDataAccess.cs
+++ TodoLibrary/DataAccess/SqlDataAccess.cs
@@ -7,13 +7,27 @@ namespace TodoLibrary.DataAccess;
 
 public class SqlDataAccess
 {
+
   private readonly IConfiguration _config;
 
+  /// <summary>
+  /// Initializes a new instance of the <see cref="SqlDataAccess"/> class.
+  /// </summary>
+  /// <param name="config">The configuration object.</param>
   public SqlDataAccess(IConfiguration config)
   {
     this._config = config;
   }
 
+  /// <summary>
+  /// Loads data from the database using a stored procedure.
+  /// </summary>
+  /// <typeparam name="T">The type of data to load.</typeparam>
+  /// <typeparam name="U">The type of parameters for the stored procedure.</typeparam>
+  /// <param name="storedProcedure">The name of the stored procedure.</param>
+  /// <param name="parameters">The parameters for the stored procedure.</param>
+  /// <param name="connectionStringName">The name of the connection string in the configuration.</param>
+  /// <returns>A list of loaded data.</returns>
   public async Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
   {
     string connectionString = _config.GetConnectionString(connectionStringName)!;
@@ -25,3 +39,4 @@ public class SqlDataAccess
     return rows.ToList();
   }
 }
+
