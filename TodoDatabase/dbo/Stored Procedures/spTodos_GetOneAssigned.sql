8f3ea69c70f76cc75b64f1a41a41d498b0ed9d43
nenad0707 <risticnenad.vr@gmail.com>
2024-03-26 13:56:49 +0100

    Added spTodos_GetOneAssigned stored procedure

---
+++ TodoDatabase/dbo/Stored Procedures/spTodos_GetOneAssigned.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spTodos_GetOneAssigned]
+	@AssignedTo INT,
+	@TodoId INT
+AS
+BEGIN
+	SELECT [Id], [Task], [AssignedTo], [IsCompleted] 
+	FROM Todos 
+	WHERE AssignedTo = @AssignedTo 
+		AND Id = @TodoId;
+END
+
