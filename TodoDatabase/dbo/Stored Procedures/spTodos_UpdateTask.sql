88bf175fc2e7428712e3bba11c33a0b5491134ee
nenad0707 <risticnenad.vr@gmail.com>
2024-03-26 14:05:42 +0100

    Added spTodos_UpdateTask stored procedure

---
+++ TodoDatabase/dbo/Stored Procedures/spTodos_UpdateTask.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spTodos_UpdateTask]
+	@Task NVARCHAR(50),
+	@AssignedTo INT,
+	@TodoId INT
+AS
+BEGIN
+	UPDATE dbo.Todos
+	SET [Task] = @Task
+	WHERE [AssignedTo] = @AssignedTo
+		AND [Id] = @TodoId;
+END
