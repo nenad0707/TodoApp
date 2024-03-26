7147801ec6ff7d30e635d9d4c005f1d8655bc60c
nenad0707 <risticnenad.vr@gmail.com>
2024-03-26 14:10:14 +0100

    Added spTodos_CompleteTodo stored procedure

---
+++ TodoDatabase/dbo/Stored Procedures/spTodos_CompleteTodo.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spTodos_CompleteTodo]
+	@AssignedTo INT,
+	@TodoId INT
+AS
+BEGIN 
+	UPDATE dbo.Todos
+	SET [IsCompleted] = 1
+	WHERE [AssignedTo] = @AssignedTo
+		AND [Id] = @TodoId;
+END
