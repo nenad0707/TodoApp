a9f3cf0d668197dbeb29acb07fa83ec6d49bdbc7
nenad0707 <risticnenad.vr@gmail.com>
2024-03-26 15:21:54 +0100

    Add SqlDataAccess service to DI container

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,3 +1,5 @@
+using TodoLibrary.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
 
 builder.Services.AddHealthChecks()
     .AddSqlServer(builder.Configuration.GetConnectionString("Default")!);
