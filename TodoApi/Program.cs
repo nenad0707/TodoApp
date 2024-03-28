99e3aa32eb2e4dab1e745f603866d6fdc79a3f16
nenad0707 <risticnenad.vr@gmail.com>
2024-03-28 23:11:57 +0100

    Update SwaggerUI configuration

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -14,7 +14,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseSwagger();
-app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty;
+});
 
 
 app.UseHttpsRedirection();
