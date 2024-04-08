51751be02d16d6c880648713d49d589f6b14613a
nenad0707 <risticnenad.vr@gmail.com>
2024-04-08 19:15:50 +0200

    Update Swagger endpoint in Program.cs to reflect Todo API version

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -16,7 +16,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API v1");
     c.RoutePrefix = string.Empty; // default is "swagger"
 });
 
