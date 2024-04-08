2d84b67d68e74ec9240977b0d159b8ee886469d4
nenad0707 <risticnenad.vr@gmail.com>
2024-04-08 19:54:32 +0200

    Update Swagger UI theme and enable static file serving in Program.cs and StartupConfig/DependencyInjectionExtensions.cs

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -18,10 +18,13 @@ app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API v1");
     c.RoutePrefix = string.Empty; // default is "swagger"
+    c.InjectStylesheet("/css/theme-material.css");
 });
 
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
 
 app.UseAuthentication();
 
