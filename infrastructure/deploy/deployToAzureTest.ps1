47a27ca85c6db2f588201bfba1bf1cfc41de7375
nenad0707 <risticnenad.vr@gmail.com>
2024-04-11 21:44:25 +0200

    Update resource group names in deployment workflow and Azure scripts

--- infrastructure/deploy/deployToAzureTest.ps1
+++ infrastructure/deploy/deployToAzureTest.ps1
@@ -10,7 +10,7 @@ $context = Get-AzSubscription -SubscriptionName PAYG-Sandboxes
 Set-AzContext $context
 
 # Set the default resource group
-Set-AzDefault -ResourceGroupName rg_sb_eastus_89803_1_171277773088
+Set-AzDefault -ResourceGroupName rg_sb_eastus_89803_1_171286435353
 ##change resourse group name
 
 # Set the GitHub organization and repository names
@@ -37,7 +37,7 @@ New-AzADAppFederatedCredential `
   -Subject "repo:$($githubOrganizationName)/$($githubRepositoryName):ref:refs/heads/main"
 
 # Get the resource group
-$resourceGroup = Get-AzResourceGroup -Name rg_sb_eastus_89803_1_171277773088
+$resourceGroup = Get-AzResourceGroup -Name rg_sb_eastus_89803_1_171286435353
 
 
 # Create a new Azure AD service principal
